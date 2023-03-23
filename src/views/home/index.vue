ce44b19b37dcf034614a90bc4f4e042a2eac604b
zhouxk <xiankaizhou>
2023-03-23 17:40:02 +0800

    cherrypick

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,14 +2,17 @@
  * @Author: zhouxk
  * @Date: 2023-02-24 14:18:34
  * @LastEditors: zhouxk
- * @LastEditTime: 2023-03-23 16:45:42
+ * @LastEditTime: 2023-03-24 13:16:05
  * @Description: file content
 -->
 <template>
   <div>hello isboyjc, This is toolsdog home page!</div>
+  <el-button type="primary">primary</el-button>
+  <div>{{person.name}}</div>
 </template>
 
 <script setup>
+const person = ref({ name: 'zxk', age: 22 })
 </script>
 
 <style scoped></style>
