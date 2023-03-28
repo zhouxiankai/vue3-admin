98c023fc899496d02b3cd810cbb2817c707e3cdd
zhouxk <xiankaizhou>
2023-03-28 10:35:38 +0800

    feature-3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Author: zhouxk
+ * @Date: 2023-02-24 13:48:46
+ * @LastEditors: zhouxk
+ * @LastEditTime: 2023-03-28 10:35:11
+ * @Description: file content
+-->
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
@@ -5,5 +12,6 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <TheWelcome />
+    <div>feature-3</div>
   </main>
 </template>
