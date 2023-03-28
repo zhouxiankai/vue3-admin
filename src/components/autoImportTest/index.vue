bcf6c03db5e0e880698cc9d43caef8a3a1990939
zhouxk <xiankaizhou>
2023-03-28 10:34:28 +0800

    feature-2

--- src/components/autoImportTest/index.vue
+++ src/components/autoImportTest/index.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Author: zhouxk
+ * @Date: 2023-02-24 16:26:48
+ * @LastEditors: zhouxk
+ * @LastEditTime: 2023-03-28 10:34:12
+ * @Description: file content
+-->
 <template>
   <div class='app-container'>
     自动导入组件
@@ -13,7 +20,9 @@ export default {
     }
   },
   computed: {},
-  created() {},
+  created() {
+    console.log('feature-2')
+  },
   methods: {}
 }
 
